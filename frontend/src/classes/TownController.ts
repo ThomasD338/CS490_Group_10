62845e05d8a01949365d89d01c7aee911902dab2
ThomasD <td338@njit.edu>
2025-12-07 18:22:23 -0500

    Linter error fix

--- frontend/src/classes/TownController.ts
+++ frontend/src/classes/TownController.ts
@@ -645,7 +645,7 @@ export default class TownController extends (EventEmitter as new () => TypedEmit
             );
           } else if (isViewingArea(eachInteractable)) {
             this._interactableControllers.push(
-              new ViewingAreaController(eachInteractable, useTownController())
+              new ViewingAreaController(eachInteractable, useTownController()),
             );
           } else if (isTicTacToeArea(eachInteractable)) {
             this._interactableControllers.push(
